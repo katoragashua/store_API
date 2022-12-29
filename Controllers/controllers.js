ac6283ee3eacf19d3f1161999b0ad280aebd9881
katoragashua <katoragashua@outlook.com>
2022-12-29 21:21:38 +0100

    Numerical filtering

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -3,7 +3,7 @@ const Product = require("../Model/productSchema");
 const CustomAPIError = require("../Middlewares/customErrorClass");
 
 const getProducts = AsyncWrapper(async (req, res, next) => {
-  const { name, company, featured, price, rating, sort } = req.query;
+  const { name, company, featured, price, rating, sort, fields, numeric_filters } = req.query;
   const queryObj = {};
   if (name) {
     queryObj.name = { $regex: name, $options: "i" }; // "i" meaning ignore case sensitivity
@@ -20,14 +20,53 @@ const getProducts = AsyncWrapper(async (req, res, next) => {
   if (featured) {
     queryObj.featured = featured === "true" ? true : false;
   }
+  if (price) {
+    queryObj.price = { $lte: Number(price) };
+  }
   console.log(queryObj);
+
+  if(numeric_filters) {
+      const operatorMap = {
+        "<": "$lt",
+        "<=": "$lte",
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq"
+      };
+
+      const regEx = /\b(<|>|<=|=>|=)\b/g
+      let filters = numeric_filters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+      const options = ["price", "rating"]
+      filters = filters.split(",").forEach(filter => {
+        const [field, operator, value] = filter.split("-");
+        if(options.includes(field)){
+          queryObj[field] = {[operator]: Number(value)};
+        }
+      })
+      console.log(queryObj)
+    }
+    
+
   let results = Product.find(queryObj);
+
   if (sort) {
-    const sortList = sort.split(',').join(" ");
+    const sortList = sort.split(",").join(" ");
     results = Product.find(queryObj).sort(sortList);
-    console.log(sortList)
   } // Optionally you can add an else condition to sort based on time product was created.
   // console.log(results);
+
+  // This is used to return only requested data fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    results = Product.find(queryObj).select(fieldsList);
+  }
+
+  const per_page = Number(req.query.per_page) || 10;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * per_page;
+  console.log(skip)
+  results = Product.find(queryObj).skip(skip).limit(per_page);
+
   const products = await results;
   // console.log(products);
   res
